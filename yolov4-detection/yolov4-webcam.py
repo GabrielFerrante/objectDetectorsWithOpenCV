2cb49e3717610fe566fbd7aa984129db7139c263
GabrielFerrante <gabrielsferrante@hotmail.com>
2023-03-17 01:22:58 -0300

    first commit

---
+++ yolov4-detection/yolov4-webcam.py
@@ -0,0 +1,100 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=False, default=0,
+	help="path to input video")
+ap.add_argument("-y", "--yolo", required=True,
+	help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applyong non-maxima suppression")
+args = vars(ap.parse_args())
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+LABELS     = open(labelsPath).read().strip().split("\n")
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([args["yolo"], "yolov4-p6.weights"])
+configPath  = os.path.sep.join([args["yolo"], "cfg.cfg"])
+# load our YOLO object detector trained on COCO dataset (80 classes)
+# and determine only the *output* layer names that we need from YOLO
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+ln  = net.getLayerNames()
+ln  = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+
+
+
+vs = cv2.VideoCapture(0)
+(W, H) = (None, None)
+rodando = True
+
+while rodando: 
+	
+	(grabbed, frame) = vs.read()
+	
+	if not grabbed: 
+		break
+	
+	if W is None or H is None: 
+		(H, W) = frame.shape[:2]
+
+	
+	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (256, 256),
+		swapRB=True, crop=False)
+	net.setInput(blob)
+	start        = time.time()
+	layerOutputs = net.forward(ln)
+	end          = time.time()
+	
+	boxes       = []
+	confidences = []
+	classIDs    = []
+
+	for output in layerOutputs: 
+		
+		for detection in output: 
+			
+			scores     = detection[5:]
+			classID    = np.argmax(scores)
+			confidence = scores[classID]
+			
+			if confidence > args["confidence"]: 
+				
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
+				
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
+				
+				boxes.append([x, y, int(width), int(height)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
+
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+		args["threshold"])
+
+	if len(idxs) > 0:
+		for i in idxs.flatten():
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
+			color = [int(c) for c in COLORS[classIDs[i]]]
+			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+			text = "{}: {:.4f}".format(LABELS[classIDs[i]],
+				confidences[i])
+			cv2.putText(frame, text, (x, y - 5),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+	if not grabbed or cv2.waitKey(1) & 0xff == ord('q'):
+		rodando = False
+	cv2.imshow("Frame", frame)
